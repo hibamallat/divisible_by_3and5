c5f69417f27219cf1ec19015415e363c1447557f
hibamallat <hibamallat040@gmail.com>
2024-10-30 17:17:45 +0200

    file updated

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-x= int(input("Enter x:"))
+#The user must enter a number x
+x= int(input("Enter x: "))
+
 for i in range (x+1):
-  if i % 3 == 0 and i % 5 == 0 :
-     print(i, end = ' ')
+    # Check if the current number i is divisible by both 3 and 5
+    if i % 3 == 0 and i % 5 == 0 : 
+        print(i, end = ' ') # Print i without a newline
